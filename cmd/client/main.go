1465295203332b4f32147f232360c75511643544
Gabriel Guesser <gdguesser@gmail.com>
2022-09-23 16:11:28 +0100

    uploading yt project to github

---
+++ cmd/client/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"hello/grpc/pb"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:9000", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := pb.NewHelloClient(conn)
+
+	req := &pb.HelloRequest{
+		Name: "John Doe",
+	}
+
+	res, err := client.SayHello(context.Background(), req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Print(res)
+}
---
+++ cmd/server/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"hello/grpc/pb"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type Server struct {
+	pb.UnimplementedHelloServer
+}
+
+func (s *Server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
+	return &pb.HelloResponse{Message: "Hello, " + in.GetName()}, nil
+}
+
+func main() {
+	println("Running gRPC server")
+
+	listener, err := net.Listen("tcp", "localhost:9000")
+	if err != nil {
+		panic(err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterHelloServer(s, &Server{})
+	if err := s.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
